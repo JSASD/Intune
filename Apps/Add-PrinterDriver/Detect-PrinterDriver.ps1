7246fd98b234b777bfce14a7b379ce48871b3a36
Quinn Henry <78760148+TheQuib@users.noreply.github.com>
2023-11-14 09:47:32 -0500

    Create detection script

---
+++ Apps/Add-PrinterDriver/Detect-PrinterDriver.ps1
@@ -0,0 +1,15 @@
+# v1.0
+# Detect-PrinterDriver.ps1
+# Detects if the HP Universal PCL 6 v7.0.1 driver is added to Windows
+# JSASD Technology Department
+
+$PrinterDriverName = "HP Universal Printing PCL 6 (v7.0.1)"
+
+$driverInstalled = Get-PrinterDriver -Name $PrinterDriverName -ErrorAction SilentlyContinue
+if ($driverInstalled) {
+    Write-Host "Installed"
+    exit 0  # Indicates presence of the driver
+} else {
+    Write-Host "Not installed"
+    exit 1  # Indicates the driver is not installed
+}
